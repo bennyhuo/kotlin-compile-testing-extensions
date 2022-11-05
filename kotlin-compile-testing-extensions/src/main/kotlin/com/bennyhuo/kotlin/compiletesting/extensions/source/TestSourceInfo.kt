eba9d4e0c26ad8a801c4a1ee92731bcbbbeb747a
bennyhuo <bennyhuo@kotliner.cn>
2022-11-05 22:45:29 +0800

    Make KotlinModule more flexible to configure different args for kapt and ksp. Support customized processors and args for different modules in FileBasedModuleInfoLoader.

--- kotlin-compile-testing-extensions/src/main/kotlin/com/bennyhuo/kotlin/compiletesting/extensions/source/TestSourceInfo.kt
+++ kotlin-compile-testing-extensions/src/main/kotlin/com/bennyhuo/kotlin/compiletesting/extensions/source/TestSourceInfo.kt
@@ -1,12 +1,32 @@
 package com.bennyhuo.kotlin.compiletesting.extensions.source
 
+import com.google.devtools.ksp.processing.SymbolProcessorProvider
+import org.jetbrains.kotlin.compiler.plugin.ComponentRegistrar
+import javax.annotation.processing.Processor
+
 class SourceModuleInfo(
     val name: String,
-    val args: Map<String, String> = emptyMap(),
-    val dependencies: List<String> = emptyList(),
+    val annotationProcessors: MutableList<String> = ArrayList(),
+    val kaptArgs: MutableMap<String, String> = HashMap(),
+    val symbolProcessorProviders: MutableList<String> = ArrayList(),
+    val kspArgs: MutableMap<String, String> = HashMap(),
+    val componentRegistrars: MutableList<String> = ArrayList(),
+    val dependencies: MutableList<String> = ArrayList(),
     val sourceFileInfos: MutableList<SourceFileInfo> = ArrayList(),
     val entries: MutableList<Entry> = ArrayList()
-)
+) {
+    @Suppress("UNCHECKED_CAST")
+    private fun <T> MutableList<String>.instantiate(): List<T> {
+        return map { SourceModuleInfo::class.java.classLoader.loadClass(it).constructors[0].newInstance() as T }
+    }
+
+    internal fun annotationProcessors() = annotationProcessors.instantiate<Processor>()
+
+    internal fun symbolProcessorProviders() = symbolProcessorProviders.instantiate<SymbolProcessorProvider>()
+
+    internal fun componentRegistrars() = componentRegistrars.instantiate<ComponentRegistrar>()
+
+}
 
 class ExpectModuleInfo(
     val name: String,
